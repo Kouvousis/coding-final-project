6cbe68083e1749fcc2dfc745c4ad425844896646
Kouvousis <dexous1996@gmail.com>
2024-12-30 20:56:40 +0200

    fixed App.jsx

--- frontend/personal-task-manager/src/App.jsx
+++ frontend/personal-task-manager/src/App.jsx
@@ -1,47 +1,38 @@
 import { useState } from "react";
-import "./App.css";
 import NavigationBar from "./components/navigation/NavigationBar";
 import TaskApp from "./components/tasks/TaskApp";
-import RegistrationForm from "./components/registration/RegistrationForm";
 import LoginForm from "./components/login/LoginForm";
+import RegistrationForm from "./components/registration/RegistrationForm";
+import UserProfile from "./components/profile/UserProfile";
 
 function App() {
-  const [currentView, setCurrentView] = useState("home");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentView, setCurrentView] = useState("home");
 
   const handleNavigation = (view) => {
     setCurrentView(view);
   };
 
-  const renderView = () => {
-    switch (currentView) {
-      case "register":
-        return <RegistrationForm />;
-      case "login":
-        return (
-          <LoginForm
-            onNavigate={handleNavigation}
-            setIsLoggedIn={setIsLoggedIn}
-          />
-        );
-      default:
-        return (
-          <main className="container">
-            <TaskApp setIsLoggedIn={setIsLoggedIn} />
-          </main>
-        );
-    }
+  const viewComponents = {
+    login: (
+      <LoginForm onNavigate={handleNavigation} setIsLoggedIn={setIsLoggedIn} />
+    ),
+    register: <RegistrationForm onNavigate={handleNavigation} />,
+    profile: <UserProfile onNavigate={handleNavigation} />,
+    home: <TaskApp isLoggedIn={isLoggedIn} />,
   };
 
   return (
-    <div className="">
-      <NavigationBar
-        onNavigate={handleNavigation}
-        isLoggedIn={isLoggedIn}
-        setIsLoggedIn={setIsLoggedIn}
-      />
-      {renderView()}
-    </div>
+    <>
+      <div className="">
+        <NavigationBar
+          onNavigate={handleNavigation}
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+        />
+        {viewComponents[currentView]}
+      </div>
+    </>
   );
 }
 
