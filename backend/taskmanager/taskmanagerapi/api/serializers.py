fd40e10f90d3dba755603d11fab1a58b30f1f3f0
Kouvousis <dexous1996@gmail.com>
2025-01-02 20:56:43 +0200

    Fixed when users updated theri profile being able to use all ready existing usernames and emails

--- backend/taskmanager/taskmanagerapi/api/serializers.py
+++ backend/taskmanager/taskmanagerapi/api/serializers.py
@@ -18,6 +18,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
         
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('Username already exists')
+        return value
+        
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
             raise serializers.ValidationError('Email already exists')
@@ -64,6 +69,18 @@ class UserUpdateSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
+        
+    def validate_username(self, value):
+        user = self.context['request'].user
+        if User.objects.filter(username=value).exclude(id=user.id).exists():
+            raise serializers.ValidationError('Username already exists')
+        return value
+
+    def validate_email(self, value):
+        user = self.context['request'].user
+        if User.objects.filter(email=value).exclude(id=user.id).exists():
+            raise serializers.ValidationError('Email already exists')
+        return value
 
     def update(self, instance, validated_data):
         if 'password' in validated_data:
